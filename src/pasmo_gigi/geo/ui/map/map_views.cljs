ff5ca9e8469f17aaaaa95a532a89715a323fb4aa
Roberto Guerra <uris77@gmail.com>
2015-09-13 20:27:12 -0600

    Removes surplus require

--- src/pasmo_gigi/geo/ui/map/map_views.cljs
+++ src/pasmo_gigi/geo/ui/map/map_views.cljs
@@ -1,21 +1,9 @@
 (ns pasmo-gigi.geo.ui.map.map-views
   (:require [re-frame.core :refer [subscribe dispatch]]
             [reagent.core :as r]
-            [goog.object :as gob]
             [pasmo-gigi.geo.ui.db :refer [settings]]))
 
 (def FEATURE-LAYER (r/atom nil))
-
-#_(defn render-map
-  []
-  (let [mapbox-api        (:mapbox-api @settings)
-        mapbox-prop       (gob/get mapbox-api "mapbox")
-        map-el            (.map mapbox-prop  "map" "uris77.nd0o07dd")
-        feature-layer-obj ((gob/get mapbox-prop "featureLayer"))
-        add-to-fn         (gob/get feature-layer-obj "addTo")
-        feature-layer     (.apply add-to-fn feature-layer-obj (array map-el))]
-    (reset! FEATURE-LAYER feature-layer)
-    (.log js/console "FEATURE_LAYER: " @FEATURE-LAYER)))
 
 (defn render-map
   []
@@ -23,8 +11,7 @@
         feature-layer (.addTo (-> js/L
                                   .-mapbox
                                   .featureLayer) map-el)]
-    (reset! FEATURE-LAYER feature-layer)
-    (.log js/console "FEATURE_LAYER: " @FEATURE-LAYER)))
+    (reset! FEATURE-LAYER feature-layer)))
 
 (defn map-coords
   [coords]
